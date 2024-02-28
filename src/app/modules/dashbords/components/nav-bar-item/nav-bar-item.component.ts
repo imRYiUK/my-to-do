4407f89355e36a5bb845dc4cf0b813ca45bf80bc
imRYiUK <imahmedtidiane@gmail.com>
2024-02-28 23:31:38 +0000

    error that i have to correct

--- src/app/modules/dashbords/components/nav-bar-item/nav-bar-item.component.ts
+++ src/app/modules/dashbords/components/nav-bar-item/nav-bar-item.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input} from '@angular/core';
+import { SvgModule } from '../../../../shared/ui/svg/svg.module';
 
 @Component({
   selector: 'app-nav-bar-item',
   standalone: true,
-  imports: [],
+  imports: [SvgModule],
   templateUrl: './nav-bar-item.component.html',
   styleUrl: './nav-bar-item.component.css'
 })
 export class NavBarItemComponent {
  @Input() name: string = "";
  @Input() className: string = "";
+ @Input() url: string = "";
 }
